ffa806154cec4a1cc17089d4c50f53bda57e7ac9
Numerica98 <00041517@uca.edu.sv>
2019-04-05 07:59:34 -0600

    Clase movie y coneccion creadas

---
+++ app/src/main/java/com/numerica98/laboratorio4/utils/NetworkUtils.kt
@@ -0,0 +1,48 @@
+package com.numerica98.laboratorio4.utils
+
+import android.net.Uri
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
+class NetworkUtils {
+
+    val MOVIES_API_URL= "http://www.omdbapi.com/"
+    val TOKEN_API = "5504e684"
+
+    fun buildSearchUrl(movieName:String): URL {
+        val builUri= Uri.parse(MOVIES_API_URL)
+            .buildUpon()
+            .appendQueryParameter("apikey", TOKEN_API)
+            .appendQueryParameter("t", movieName)
+            .build()
+
+        return try {
+            URL(builUri.toString())
+        }catch (e:MalformedURLException){
+            URL("")
+        }
+    }
+
+    @Throws(IOException::class)
+    fun getResponseFRomHttpUrl(url:URL): String{
+        val urlConnection = url.openConnection() as HttpURLConnection
+        try {
+            val `in` = urlConnection.inputStream
+
+            val scanner = Scanner(`in`)
+            scanner.useDelimiter("\\A")
+
+            val hasInput = scanner.hasNext()
+            return if (hasInput) {
+                scanner.next()
+            } else {
+                ""
+            }finally {
+                urlConnection.disconnect()
+            }
+        }
+    }
+}
